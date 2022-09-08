6b55a890f46e98974c52d757024caa3cce78bb70
“mastermind717” <halmanimkd@gmail.com>
2022-09-08 16:54:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ *main-Entry point
+ *
+ *Return: Always 1 (success).
  */
-
 int main(void)
-
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015 10-19\n", 61;
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
-
