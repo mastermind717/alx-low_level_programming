dd3512ae7437d8f8aa46c93e77167ff5001138d7
“mastermind717” <halmanimkd@gmail.com>
2022-09-15 06:28:40 -0700

    nest n code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - A function that multiplies two integers.
+* @a: First input
+* @b: Second input
+* Return: Multiplication result of the 2 input.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
