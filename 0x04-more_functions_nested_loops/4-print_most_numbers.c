04be28be45c8299dd369299f65bb7d56a237fb22
“mastermind717” <halmanimkd@gmail.com>
2022-09-15 07:44:04 -0700

    nest these eggs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* print_most_numbers - print most except 2 and 4
-*
-* Return: void
-*/
+ * print_most_numbers - prints most except 2 and 4
+ *
+ * Return: void
+ */
 void print_most_numbers(void)
 {
 	int i;
 
-	for ((i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if ((i == 2) || (i == 4))
 		{
@@ -16,6 +16,5 @@ void print_most_numbers(void)
 		}
 		_putchar('0' + i);
 	}
-	_putchar('\n')
-
-
+	_putchar('\n');
+}
