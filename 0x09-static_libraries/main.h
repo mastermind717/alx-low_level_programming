1fd38cca78a5c9985e21c377d1a222419ea408a6
“mastermind717” <halmanimkd@gmail.com>
2022-09-30 07:11:45 -0700

    STATIC

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef _HOLBERTON_H_
-
-#define _HOLBERTON_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,3 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
